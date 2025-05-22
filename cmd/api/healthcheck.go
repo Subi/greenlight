ba02da30ca6f7cf1e0eee64a93a20b53cd935eba
Subi <subi@Judaeas-MacBook-Pro.local>
2025-05-22 06:44:36 -0400

    udpate json response via healthcheck endpoint

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
+var ErrorProcessing = "Server encountered an error trying to process the request"
+
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Status: available")
-	fmt.Fprintf(w, "Environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "Version: %v\n", app.config.port)
+
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	v, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Error("Error marshalling data", err)
+		http.Error(w, ErrorProcessing, http.StatusInternalServerError)
+		return
+	}
+
+	// Add a new line to JSON for better readability in terminals
+	v = append(v, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(v)
 }
