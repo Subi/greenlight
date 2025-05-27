f88fb25240e3a6bb8f136caa98fdb3c58867b8d1
Subi <subi@Judaeas-MacBook-Pro.local>
2025-05-27 13:57:13 -0400

    second commit

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,30 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-var ErrorProcessing = "Server encountered an error trying to process the request"
-
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	data := envelope{
+		"message": "OK",
+		"status":  "Available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
-
-	v, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		app.logger.Error("Error marshalling data", err)
-		http.Error(w, ErrorProcessing, http.StatusInternalServerError)
-		return
+		app.serverErrorResponse(w, r, nil)
 	}
-
-	// Add a new line to JSON for better readability in terminals
-	v = append(v, '\n')
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(v)
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,20 +2,48 @@ package main
 
 import (
 	"fmt"
+	"github.com/subi/greenlight/internal/data"
 	"net/http"
+	"time"
 )
 
-func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create movie")
+type input struct {
+	Title   string   `json:"title"`
+	Year    int      `json:"year"`
+	Runtime int32    `json:"runtime"`
+	Genres  []string `json:"genres"`
 }
 
-func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	input := &input{}
 
-	id, err := app.readIdParams(r)
+	err := app.readJSON(r, w, input)
 	if err != nil {
-		http.NotFound(w, r)
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
-	fmt.Fprintln(w, "show movie details", id)
+	fmt.Fprintf(w, "%+v\n", input)
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIdParams(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Focus",
+		Year:      2015,
+		Runtime:   120,
+		Genres:    []string{"drama", "crime", "mystery"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
